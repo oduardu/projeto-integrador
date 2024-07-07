2c66f67a50a3b405dabab5b17c3e01eaa36cf63f
Filipe M. Almeida <filipemedeirosalmeida@gmail.com>
2024-07-06 22:36:11 -0300

    feat: criado crud de fornecedor e produto + mostrando clientes na tabela

--- api/src/cruds/client.js
+++ api/src/cruds/client.js
@@ -1,7 +1,7 @@
 const { db } = require("../database/connection")
 
 exports.addClient = async (req, res) => {
-    const { cpf, cnpj, name, street, number, city, state, phone } = req.body;
+    const { cpf, cnpj, name, street, number, city, state, phone, email } = req.body;
 
     if (!cpf && !cnpj) {
         return res.status(400).json({
@@ -11,8 +11,8 @@ exports.addClient = async (req, res) => {
     }
 
     try {
-        await db.none('INSERT INTO cliente (cpf, cnpj, nome, rua, numero, cidade, estado, telefone) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', 
-            [cpf, cnpj, name, street, number, city, state, phone]);
+        await db.none('INSERT INTO cliente (cpf, cnpj, nome, rua, numero, cidade, estado, telefone, email) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)', 
+            [cpf, cnpj, name, street, number, city, state, phone, email]);
 
         res.status(201).json({title: 'Sucesso', description: 'Cliente cadastrado com sucesso'
         });
@@ -52,19 +52,20 @@ exports.updateClient = async (req, res) => {
     const identifier = req.params.identifier; 
 
     try {
-        const clienteExistente = await db.oneOrNone('SELECT * FROM cliente WHERE cpf = $1 OR cnpj = $1', identifier);
+        const existentClient = await db.oneOrNone('SELECT * FROM cliente WHERE cpf = $1 OR cnpj = $1', identifier);
 
-        const novoCliente = {
-            name: req.body.name || clienteExistente.nome,
-            phone: req.body.phone || clienteExistente.telefone,
-            street: req.body.street || clienteExistente.rua,
-            number: req.body.number || clienteExistente.numero,
-            city: req.body.city || clienteExistente.cidade,
-            state: req.body.state || clienteExistente.estado
+        const updatedClient = {
+            name: req.body.name || existentClient.nome,
+            email: req.body.email || existentClient.email, 
+            phone: req.body.phone || existentClient.telefone,
+            street: req.body.street || existentClient.rua,
+            number: req.body.number || existentClient.numero,
+            city: req.body.city || existentClient.cidade,
+            state: req.body.state || existentClient.estado
         };
 
-        await db.none('UPDATE cliente SET nome = $1, telefone = $2, rua = $3, numero = $4, cidade = $5, estado = $6 WHERE cpf = $7 OR cnpj = $7',
-            [novoCliente.name, novoCliente.phone, novoCliente.street, novoCliente.number, novoCliente.city, novoCliente.state, identifier]);
+        await db.none('UPDATE cliente SET nome = $1, telefone = $2, rua = $3, numero = $4, cidade = $5, estado = $6, email = $7 WHERE cpf = $8 OR cnpj = $8',
+            [updatedClient.name, updatedClient.phone, updatedClient.street, updatedClient.number, updatedClient.city, updatedClient.state, novoCliente.email, identifier]);
 
         res.status(200).json({ title: 'Sucesso', description: 'Cliente atualizado com sucesso' });
     } catch (error) {
--- api/src/cruds/supplier.js
+++ api/src/cruds/supplier.js
@@ -1,21 +1,77 @@
 const { db } = require("../database/connection")
 
-// CREATE
 exports.addSupplier = async (req, res) => {
-    console.log("Adicionando fornecedor")
-}
+    const { cnpj, name, street, number, city, state } = req.body;
+
+    try {
+        await db.none('INSERT INTO produto (cnpj, nome, rua, numero, cidade, estado) VALUES ($1, $2, $3, $4, $5, $6)', 
+            [cnpj, name, street, number, city, state]);
+
+        res.status(201).json({title: 'Sucesso', description: 'Fornecedor cadastrado com sucesso'});
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            title: 'Erro',
+            description: 'Erro ao cadastrar produto'
+        });
+    }
+};
 
 // READ
-exports.getSupplier = async (req, res) => {
-   console.log("Listando fornecedor(es)")
-}
+exports.getAllSuppliers = async (req, res) => {
+    try {
+        const suppliers = await db.any('SELECT * FROM fornecedor');
+        res.status(200).json(suppliers);
+    } catch (error) {
+        console.error('Erro ao listar todos os fornecedores:', error);
+        res.status(500).json({ message: 'Erro ao listar todos os fornecedores' });
+    }
+};
 
-// UPDATE
+exports.getSupplierByName = async (req, res) => {
+    const { name } = req.query;
+    try {
+        const fornecedores = await db.any('SELECT * FROM fornecedor WHERE nome ILIKE $1', [`%${name}%`]);
+        res.status(200).json(fornecedores);
+    } catch (error) {
+        console.error('Erro ao listar fornecedores por nome:', error);
+        res.status(500).json({ message: 'Erro ao listar fornecedores por nome' });
+    }
+};
+
 exports.updateSupplier = async (req, res) => {
-    console.log("Atualizando fornecedor")
-}
+    const identifier = req.params.identifier; 
 
-// DELETE
+    try {
+        const existentSupplier = await db.oneOrNone('SELECT * FROM fornecedor WHERE cnpj = $1', identifier);
+
+        const updatedSupplier = {
+            name: req.body.name || existentSupplier.nome,
+            street: req.body.street || existentSupplier.rua,
+            number: req.body.number || existentSupplier.numero,
+            city: req.body.city || existentSupplier.cidade,
+            state: req.body.state || existentSupplier.estado,
+        };
+
+        await db.none('UPDATE fornecedor SET nome = $1, rua = $2, numero = $3, cidade = $4, estado = $5, WHERE cnpj = $6',
+            [updatedSupplier.name, updatedSupplier.street, updatedSupplier.number, updatedSupplier.city, updatedSupplier.state, identifier]);
+
+        res.status(200).json({ title: 'Sucesso', description: 'Fornecedor atualizado com sucesso' });
+    } catch (error) {
+        console.error('Erro ao atualizar fornecedor:', error);
+        res.status(500).json({title: 'Erro', description: 'Erro ao atualizar fornecedor' });
+    }
+};
+
 exports.deleteSupplier = async (req, res) => {
-    console.log("Removendo fornecedor")
-}
+    const identifier = req.params.identifier; 
+
+    try {
+        await db.none('DELETE FROM fornecedor WHERE cnpj = $1', identifier);
+        res.status(200).json({ title: 'Sucesso', description: 'Fornecedor removido com sucesso' });
+    } catch (error) {
+        console.error('Erro ao remover fornecedor:', error);
+        res.status(500).json({ title: 'Erro', description: 'Erro ao remover fornecedor' });
+    }
+};

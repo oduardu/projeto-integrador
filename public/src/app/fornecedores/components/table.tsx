9bccdcd7bcc0a60820eae0a3e050314bb18cea36
Filipe M. Almeida <filipemedeirosalmeida@gmail.com>
2024-07-08 16:44:52 -0300

    refactor: colocado os estados do Brasil em um arquivo separado para evitar repetição de código

--- public/src/app/fornecedores/components/table.tsx
+++ public/src/app/fornecedores/components/table.tsx
@@ -119,7 +119,7 @@ export const DataTable: React.FC = () => {
           </Button>
         );
       },
-      cell: ({ row }) => <div>{row.getValue<number>("cnpj").toString().replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3\\$4-$5")}</div>,
+      cell: ({ row }) => <div>{row.getValue<string>("cnpj").replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5")}</div>,
     },
     {
       accessorKey: "cidade",
--- public/src/components/partials/cliente/form-cadastro.tsx
+++ public/src/components/partials/cliente/form-cadastro.tsx
@@ -1,3 +1,4 @@
+import { EstadosBrasil } from "@/components/estados-brasil";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -33,35 +34,6 @@ const formSchema = z.object({
 
 export function FormCadastro() {
   const { toast } = useToast();
-  const EstadosBrasil = [
-    { value: "AC", label: "Acre" },
-    { value: "AL", label: "Alagoas" },
-    { value: "AP", label: "Amapá" },
-    { value: "AM", label: "Amazonas" },
-    { value: "BA", label: "Bahia" },
-    { value: "CE", label: "Ceará" },
-    { value: "DF", label: "Distrito Federal" },
-    { value: "ES", label: "Espírito Santo" },
-    { value: "GO", label: "Goiás" },
-    { value: "MA", label: "Maranhão" },
-    { value: "MT", label: "Mato Grosso" },
-    { value: "MS", label: "Mato Grosso do Sul" },
-    { value: "MG", label: "Minas Gerais" },
-    { value: "PA", label: "Pará" },
-    { value: "PB", label: "Paraíba" },
-    { value: "PR", label: "Paraná" },
-    { value: "PE", label: "Pernambuco" },
-    { value: "PI", label: "Piauí" },
-    { value: "RJ", label: "Rio de Janeiro" },
-    { value: "RN", label: "Rio Grande do Norte" },
-    { value: "RS", label: "Rio Grande do Sul" },
-    { value: "RO", label: "Rondônia" },
-    { value: "RR", label: "Roraima" },
-    { value: "SC", label: "Santa Catarina" },
-    { value: "SP", label: "São Paulo" },
-    { value: "SE", label: "Sergipe" },
-    { value: "TO", label: "Tocantins" },
-  ];
 
   const [identifier, changeIdentifier] = useState('cpf');
   const [selectedState, setSelectedState] = useState("AC");
